5576797a70cf86040603a0cb1f0c11d96b872a4a
Godfred Hammond <godfred.hammond2018@yahoo.com>
2024-10-17 11:41:28 +0000

    just a minimal correction of the onClick showing on the LoginPopup panel

--- tasti_fries_webapp/frontend/src/components/LoginPopup/LoginPopup.jsx
+++ tasti_fries_webapp/frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -26,7 +26,7 @@ const LoginPopup = ({ setShowLogin }) => {
         ?<p>Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
         :<p>Already have an account? <span onClick={()=>setCurrState("Login")}>Login here</span></p>
         }
-         onClick={}
+        
       </form>
     </div>
   );
