df127c6a81bc81fc982f776e112134b145704a79
Godfred Hammond <godfred.hammond2018@yahoo.com>
2024-10-18 09:12:51 +0000

    Implemented the same no delivery charge if there is no item in the cart logic from PlaceOrder.jsx

--- tasti_fries_webapp/frontend/src/pages/Cart/Cart.jsx
+++ tasti_fries_webapp/frontend/src/pages/Cart/Cart.jsx
@@ -52,12 +52,12 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>₵{20}</p>
+              <p>₵{getTotalCartAmount()===0?0:20}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>₵{getTotalCartAmount()+2}</b>
+              <b>₵{getTotalCartAmount()===0?0:getTotalCartAmount()+20}</b>
             </div>
           </div>
           <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
